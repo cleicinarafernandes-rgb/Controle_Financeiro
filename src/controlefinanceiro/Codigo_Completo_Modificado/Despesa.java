bd0d29ddcbc353737494b3268b702ef37dbd086e
carolinaasouza939-svg <carolinaasouza939@gmail.com>
2026-01-09 19:02:42 -0300

    Add Despesa class for expense transactions

---
+++ src/controlefinanceiro/Codigo_Completo_Modificado/Despesa.java
@@ -0,0 +1,19 @@
+package Controle_Financeiro;
+
+import java.time.LocalDate;
+
+public class Despesa extends Transacao {
+
+    public Despesa(double valor, String descricao){
+        super(valor, descricao);
+    }
+
+    public Despesa(double valor, String descricao, LocalDate data, String categoria) {
+        super(valor, descricao, data, categoria);
+    }
+
+    @Override
+    public String getTipo() {
+        return "Despesa";
+    }
+}
