16cd4b3f5656c8f24f5aa06d74e25e662950c020
carolinaasouza939-svg <carolinaasouza939@gmail.com>
2026-01-09 19:03:43 -0300

    Add Gerenciador_Financas class for financial management

---
+++ src/controlefinanceiro/Codigo_Completo_Modificado/Gerenciador_Finanças.java
@@ -0,0 +1,89 @@
+package Controle_Financeiro;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gerenciador_Financas {
+
+    private List<Transacao> transacoes;
+
+    public Gerenciador_Financas(){
+        transacoes = new ArrayList<>();
+    }
+
+    public void adicionarReceita(double valor, String descricao){
+        if (valor <= 0) {
+            throw new IllegalArgumentException();
+        }
+        transacoes.add(new Receita(valor, descricao));
+    }
+
+    public void adicionarReceita(double valor, String descricao, LocalDate data, String categoria) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException();
+        }
+        transacoes.add(new Receita(valor, descricao, data, categoria));
+    }
+
+    public void adicionarDespesa(double valor, String descricao) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException();
+        }
+        transacoes.add(new Despesa(valor, descricao));
+    }
+
+    public void adicionarDespesa(double valor, String descricao, LocalDate data, String categoria) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException();
+        }
+        transacoes.add(new Despesa(valor, descricao, data, categoria));
+    }
+
+    public double calcularSaldo() {
+        double saldo = 0;
+
+        for (Transacao t : transacoes) {
+            if (t.getTipo().equals("Receita")) {
+                saldo += t.getValor();
+            } else {
+                saldo -= t.getValor();
+            }
+        }
+
+        return saldo;
+    }
+
+    public List<Transacao> filtrarPorCategoria(String categoria) {
+        List<Transacao> resultado = new ArrayList<>();
+
+        for (Transacao t : transacoes) {
+            if (t.getCategoria().equalsIgnoreCase(categoria)) {
+                resultado.add(t);
+            }
+        }
+
+        return resultado;
+    }
+
+    public List<Transacao> filtrarPorMes(int mes, int ano) {
+        List<Transacao> resultado = new ArrayList<>();
+
+        for (Transacao t : transacoes) {
+            if (t.getData().getMonthValue() == mes &&
+                    t.getData().getYear() == ano) {
+                resultado.add(t);
+            }
+        }
+
+        return resultado;
+    }
+
+    public List<Transacao> getTransacoes() {
+        return transacoes;
+    }
+
+    public void setTransacoes(List<Transacao> transacoes) {
+        this.transacoes = transacoes;
+    }
+}
